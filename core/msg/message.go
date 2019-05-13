b7c7c686860e48d28ce67ff014593e44bd66341f
张永红 <huike@tuya.com>
2019-05-13 15:11:46 +0800

    decode batch message

--- core/msg/message.go
+++ core/msg/message.go
@@ -15,6 +15,9 @@ package msg
 
 import (
 	"bytes"
+	"io"
+	"encoding/binary"
+	"errors"
 
 	"github.com/golang/protobuf/proto"
 	"github.com/wolfstudy/pulsar-client-go/pkg/api"
@@ -37,4 +40,55 @@ func (m *Message) Equal(other *Message) bool {
 		proto.Equal(m.Msg, other.Msg) &&
 		proto.Equal(m.Meta, other.Meta) &&
 		bytes.Equal(m.Payload, other.Payload)
+}
+
+type SingleMessage struct {
+	SingleMetaSize uint32
+	SingleMeta     *api.SingleMessageMetadata
+	SinglePayload  []byte
+}
+
+func DecodeBatchMessage(msg *Message)([]*SingleMessage, error){
+	num := msg.Meta.GetNumMessagesInBatch()
+	if  num==0{
+		return nil,errors.New("num_message_in_batch is nil or 0")
+	}
+	return DecodeBatchPayload(msg.Payload,num)
+}
+
+
+
+// DecodeBatchPayload 解析batch类型的payload
+// 如果生产者推送的时候，使用了batch功能，msg.Payload将是一个[]SingleMessage结构
+func DecodeBatchPayload(bp []byte, batchNum int32) ([]*SingleMessage, error) {
+	buf32 := make([]byte, 4)
+	rdBuf := bytes.NewReader(bp)
+	list := make([]*SingleMessage, 0, batchNum)
+	for i := int32(0); i < batchNum; i++ {
+		// singleMetaSize
+		if _, err := io.ReadFull(rdBuf, buf32); err != nil {
+			return nil, err
+		}
+		singleMetaSize := binary.BigEndian.Uint32(buf32)
+		// singleMeta
+		singleMetaBuf := make([]byte, singleMetaSize)
+		if _, err := io.ReadFull(rdBuf, singleMetaBuf); err != nil {
+			return nil, err
+		}
+		singleMeta := new(api.SingleMessageMetadata)
+		if err := proto.Unmarshal(singleMetaBuf, singleMeta); err != nil {
+			return nil, err
+		}
+		// payload
+		singlePayload := make([]byte, singleMeta.GetPayloadSize())
+		if _, err := io.ReadFull(rdBuf, singlePayload); err != nil {
+			return nil, err
+		}
+		d := &SingleMessage{}
+		d.SingleMetaSize = singleMetaSize
+		d.SingleMeta = singleMeta
+		d.SinglePayload = singlePayload
+		list = append(list, d)
+	}
+	return list, nil
 }
