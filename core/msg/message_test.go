d25a3276ed403dfd6e13447a687ad6d0fcb8727a
张永红 <huike@tuya.com>
2019-05-13 23:53:15 +0800

    go format for message_test.go

--- core/msg/message_test.go
+++ core/msg/message_test.go
@@ -1,21 +1,21 @@
 package msg
 
-import(
+import (
 	"testing"
 )
 
-func TestDecodeBatchPayload(t *testing.T){
-	payload := []byte{0,0,0,2,24,12,104,101,108,108,111,45,112,117,108,115,97,114} // hello-pulsar
-	list,err := DecodeBatchPayload(payload,1)
-	if err!=nil{
+func TestDecodeBatchPayload(t *testing.T) {
+	payload := []byte{0, 0, 0, 2, 24, 12, 104, 101, 108, 108, 111, 45, 112, 117, 108, 115, 97, 114} // hello-pulsar
+	list, err := DecodeBatchPayload(payload, 1)
+	if err != nil {
 		t.Fatal(err)
 	}
-	if get,want := len(list),1;get!=want{
-		t.Errorf("want %v, but get %v",get,want)
+	if get, want := len(list), 1; get != want {
+		t.Errorf("want %v, but get %v", get, want)
 	}
-	
+
 	m := list[0]
-	if get,want := string(m.SinglePayload), "hello-pulsar";get!=want{
-		t.Errorf("want %v, but get %v",get,want)
+	if get, want := string(m.SinglePayload), "hello-pulsar"; get != want {
+		t.Errorf("want %v, but get %v", get, want)
 	}
 }
