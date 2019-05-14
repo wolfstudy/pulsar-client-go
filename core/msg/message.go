8f5aa7436be5e38d7782ffd9df243f24baddc574
faker <570453280@qq.com>
2019-05-14 09:48:25 +0800

    decode batch message (#48)
    * decode batch message
    
    Provides a method for parsing the payload generated by the producer using the batch produce option.

--- core/msg/message.go
+++ core/msg/message.go
@@ -15,6 +15,9 @@ package msg
 
 import (
 	"bytes"
+	"encoding/binary"
+	"errors"
+	"io"
 
 	"github.com/golang/protobuf/proto"
 	"github.com/wolfstudy/pulsar-client-go/pkg/api"
@@ -37,4 +40,55 @@ func (m *Message) Equal(other *Message) bool {
 		proto.Equal(m.Msg, other.Msg) &&
 		proto.Equal(m.Meta, other.Meta) &&
 		bytes.Equal(m.Payload, other.Payload)
+}
+
+// SingleMessage represents one of the elements of the batch type payload
+type SingleMessage struct {
+	SingleMetaSize uint32
+	SingleMeta     *api.SingleMessageMetadata
+	SinglePayload  []byte
+}
+
+// DecodeBatchMessage decode message if num_messages_in_batch exist and bigger than 0
+func DecodeBatchMessage(msg *Message) ([]*SingleMessage, error) {
+	num := msg.Meta.GetNumMessagesInBatch()
+	if num == 0 {
+		return nil, errors.New("num_message_in_batch is nil or 0")
+	}
+	return DecodeBatchPayload(msg.Payload, num)
+}
+
+// DecodeBatchPayload parses the payload of the batch type
+// If the producer uses the batch function, msg.Payload will be a SingleMessage array structure.
+func DecodeBatchPayload(bp []byte, batchNum int32) ([]*SingleMessage, error) {
+	buf32 := make([]byte, 4)
+	rdBuf := bytes.NewReader(bp)
+	list := make([]*SingleMessage, 0, batchNum)
+	for i := int32(0); i < batchNum; i++ {
+		// singleMetaSize
+		if _, err := io.ReadFull(rdBuf, buf32); err != nil {
+			return nil, err
+		}
+		singleMetaSize := binary.BigEndian.Uint32(buf32)
+		// singleMeta
+		singleMetaBuf := make([]byte, singleMetaSize)
+		if _, err := io.ReadFull(rdBuf, singleMetaBuf); err != nil {
+			return nil, err
+		}
+		singleMeta := new(api.SingleMessageMetadata)
+		if err := proto.Unmarshal(singleMetaBuf, singleMeta); err != nil {
+			return nil, err
+		}
+		// payload
+		singlePayload := make([]byte, singleMeta.GetPayloadSize())
+		if _, err := io.ReadFull(rdBuf, singlePayload); err != nil {
+			return nil, err
+		}
+		d := &SingleMessage{}
+		d.SingleMetaSize = singleMetaSize
+		d.SingleMeta = singleMeta
+		d.SinglePayload = singlePayload
+		list = append(list, d)
+	}
+	return list, nil
 }
